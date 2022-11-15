dc93d4b85c496ef9dcc2af007572329ef9154bc6
jszama <jmszamotulski@gmail.com>
2022-11-15 13:01:55 +0000

    BACKEND GAME COMPLETE

--- game.py
+++ game.py
@@ -1,7 +1,5 @@
-mineField = [["X" for x in range(6)] for i in range(6)]
-viewableField = [["X" for x in range(6)] for i in range(6)]
 import random
-game = True
+
 
 class Mine():
     def __init__(self):
@@ -10,80 +8,72 @@ class Mine():
     def explode(self):
         print("END GAME. YOU STEPPED ON A MINE!")
 
-def refresh():
-    for i in range(len(viewableField[0])):
-        print(viewableField[i])
-    print("--------------------------------------")
 
-def start():
-    for i in range(4):
-        mineField[random.randint(0,5)][random.randint(0,5)] = Mine()
-    refresh()
+class Grid():
+    def __init__(self):
+        self.rows = 6
+        self.cols = 6
+        self.mineField = [["X" for x in range(6)] for i in range(6)]
+        self.viewableField = [["X" for x in range(6)] for i in range(6)]
+        
+        self.game = True
+        
+        for i in range(4):
+            self.mineField[random.randint(0,5)][random.randint(0,5)] = Mine()
+
+        
+        while self.game == True:
+            self.refresh()
+            try:
+                self.click(int(input("Row"))-1,int(input("Col"))-1)
+            except:
+                print("Wrong entry")
+
+    def refresh(self):
+        for i in range(len(self.viewableField[0])):
+            print(self.viewableField[i])
+        print("--------------------------------------")
 
 
-def checkArond(row,col):
-    nearby = 0
-    if mineField[row][col] != "X":
-        mineField[row][col].explode()
-        viewableField[row][col] = "BOOM!"
-        game = False
-    try:
-        mineField[row-1][col-1] != "X"
-        nearby +=1
-        viewableField[row][col] = str(nearby)
-    except IndexError:
-        nearby -=1
-    try:    
-        mineField[row-1][col] != "X"
-        nearby += 1
-        viewableField[row][col] = str(nearby)
-    except IndexError:
-        nearby -=1
-    try:
-        mineField[row-1][col+1] != "X"
-        nearby +=1
-        viewableField[row][col] = str(nearby)
-    except IndexError:
-        nearby -=1
-    try:
-        mineField[row][col-1] != "X"
-        nearby +=1
-        viewableField[row][col] = str(nearby)
-    except IndexError:
-        nearby -=1
-    try:
-        mineField[row][col+1] != "X"
-        nearby +=1
-        viewableField[row][col] = str(nearby)
-    except IndexError:
-        nearby -=1
-    try:
-        mineField[row+1][col-1] != "X"
-        nearby +=1
-        viewableField[row][col] = str(nearby)
-    except IndexError:
-        nearby -=1
-    try:
-        mineField[row+1][col] != "X"
-        nearby +=1
-        viewableField[row][col] = str(nearby)
-    except IndexError:
-        nearby -=1
-    try:
-        mineField[row+1][col+1] != "X"
-        nearby +=1
-        viewableField[row][col] = str(nearby)
-    except IndexError:
-        nearby -=1
+    def checkArond(self,reqRow,reqCol):
+        nearby = 0
+        if self.mineField[reqRow][reqCol] != "X":
+            self.mineField[reqRow][reqCol].explode()
+            self.viewableField[reqRow][reqCol] = "BOOM!"
+            self.game = False
+        else:
+            valid_rows = [reqRow]
+            valid_cols = [reqCol]
+            
+            if 0 < reqRow < 5:
+                valid_rows.append(reqRow - 1)
+                valid_rows.append(reqRow + 1)
+            elif reqRow == 0:
+                valid_rows.append(reqRow + 1)
+            elif reqRow == 5:
+                valid_rows.append(reqRow - 1)
+                
+            if 0 < reqCol < 5:
+                valid_cols.append(reqCol - 1) 
+                valid_cols.append(reqCol + 1)
+            elif reqCol == 0:
+                valid_cols.append(reqCol + 1)
+            elif reqCol == 5:
+                valid_cols.append(reqCol -1)
+            
+            for R in valid_rows:
+                print(R)
+                for C in valid_cols:
+                    print(C)
+                    if self.mineField[R][C] != "X":
+                        nearby += 1
+            
+            self.viewableField[reqRow][reqCol] = str(nearby)
 
     
-def click(row,col):
-    
-    checkArond(row,col)
-    
-    refresh()
+    def click(self,reqRow,reqCol):
+        
+        self.checkArond(reqRow,reqCol)
+        
 
-start()
-
-while game:
-    click(int(input("Row"))-1,int(input("Col"))-1)
+game = Grid()
